b469fe6d8b78c060d18ddb573a8ed7c2829663cc
Matheus Carmo <mematheuslc@gmail.com>
2017-05-02 17:15:43 -0300

    feat(item): Add alumni item

--- where_are_you_going/items.py
+++ where_are_you_going/items.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
-class WhereAreYouGoingItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Alumni(scrapy.Item):
+    name = scrapy.Field()
+    registry = scrapy.Field()
